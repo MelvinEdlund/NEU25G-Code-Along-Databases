f1bdd482db0385a2da12bd2e7da61ec6232157bc
Melvinedl <medlund01@gmail.com>
2025-11-21 11:54:54 +0100

    009, 010, 011

--- 009_TempTables.sql
+++ 009_TempTables.sql
@@ -1,3 +1,6 @@
+--- Temporära tabeller ----------------
+
+select * from #tempUsers;  
 
 select 
 	id, 
@@ -8,6 +11,20 @@ into
 from 
 	Users 
 where 
-	FirstName like 'a%';
+	FirstName like 'a%'
 
 select * from #tempUsers;
+
+--- Tabell-variabel ---------------
+
+declare @people as table
+(
+	name nvarchar(MAX),
+	age int
+);
+
+insert into @people values('Anna', 29);
+insert into @people values('Frida', 24);
+insert into @people values('Erik', 35);
+
+select * from @people;
